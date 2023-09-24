e801760145900f6b3c2e1a6bcad5bba072deda5f
Jawad Ali <jawad751@gmail.com>
2023-09-24 16:06:10 +0800

    User Registration

---
+++ CRM/webcrm/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+from django import forms
+
+# create user
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model= User
+        fields= ['username', 'email', 'password1', 'password2']
+
+# Authenticate a user
+
+class LoginForm(AuthenticationForm):
+    username= forms.CharField(widget=TextInput(), max_length=150, required=True)
+    password= forms.CharField(widget=PasswordInput(), max_length=150, required=True)
+
+    
--- CRM/webcrm/views.py
+++ CRM/webcrm/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout,login
 from django.contrib import messages
+from .forms import UserCreationForm, LoginForm
+from django.contrib.auth.models import auth
+
 
 # Create your views here.
 
@@ -26,14 +29,47 @@ def home(request):
 	# 	return render(request, 'webcrm/home.html',{} )
 
 def register(request):
-    pass
+    form= UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('dashboard')
+    
+    context={'form':form}
+    return render(request, 'register.html', context=context)
 
-def login_user(request):
-    pass
+
+def user_login(request):
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("dashboard")
+
+
+    context = {'form': form}
+
+    return render(request, 'my-login.html', context=context)
+    
 
 def dashboard(request):
     pass
 
-def logout_user(request):
+def user_logout(request):
     pass
 
