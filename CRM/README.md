84902deec85e875e9acde1271fb60878f0286398
Jawad Ali <jawad751@gmail.com>
2023-09-24 13:00:38 +0800

    Project debug

---
+++ CRM/README.md
@@ -0,0 +1,2 @@
+# Customer-Relationship-Management-Tool
+A customer relationship management tool created in Django and mySQL database
--- CRM/webcrm/views.py
+++ CRM/webcrm/views.py
@@ -6,23 +6,32 @@ from django.contrib import messages
 
 
 def home(request):
+    return render(request, 'home.html')
+
+
 	# Check to see if logging in
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		# Authenticate
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			messages.success(request, "You Have Been Logged In!")
-			return redirect('home')
-		else:
-			messages.success(request, "There Was An Error Logging In, Please Try Again...")
-			return redirect('home')
-	else:
-		return render(request, 'webcrm/home.html',{} )
+	# if request.method == 'POST':
+	# 	username = request.POST['username']
+	# 	password = request.POST['password']
+	# 	# Authenticate
+	# 	user = authenticate(request, username=username, password=password)
+	# 	if user is not None:
+	# 		login(request, user)
+	# 		messages.success(request, "You Have Been Logged In!")
+	# 		return redirect('home')
+	# 	else:
+	# 		messages.success(request, "There Was An Error Logging In, Please Try Again...")
+	# 		return redirect('home')
+	# else:
+	# 	return render(request, 'webcrm/home.html',{} )
+
+def register(request):
+    pass
 
 def login_user(request):
+    pass
+
+def dashboard(request):
     pass
 
 def logout_user(request):
